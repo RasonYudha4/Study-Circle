40335ed794abec501f2f2268ce3ac0e246d2092d
rasonyudha4 <rasonyudha921@gmail.com>
2024-12-05 20:35:24 +0700

    Adding card for notification

--- lib/notification/view/notication_page.dart
+++ lib/notification/view/notication_page.dart
@@ -10,34 +10,129 @@ class NotificationPage extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(vertical: 35, horizontal: 25),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
             alignment: Alignment.bottomCenter,
-            height: 120,
+            height: 90,
             decoration: BoxDecoration(color: Color(0xFF127369)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  // mainAxisSize: MainAxisSize.min,
-                  // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Notifications",
-                      style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    )
-                  ],
+            child: Row(children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  size: 28,
+                  color: Colors.white,
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "Notifications",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              )
+            ]),
           ),
           SizedBox(
             height: 15,
           ),
+          Expanded(
+            child: ListView(padding: EdgeInsets.all(15), children: [
+              NotificationCard(),
+              SizedBox(
+                height: 20,
+              ),
+              NotificationCard(),
+            ]),
+          )
         ],
       ),
+    );
+  }
+}
+
+class NotificationCard extends StatelessWidget {
+  const NotificationCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 110,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xFF10403B),
+              ),
+              child: Stack(
+                // Use Stack to overlay the text
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 20, // Fixed width for the left side
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20),
+                          ),
+                          color: Color(0xFF10403B), // First color
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                            color: Color(0xFFBFBFBF), // Second color
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12, left: 40),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Study Circle",
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF127369)),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Container(
+                          width: 200,
+                          child: Text(
+                            "Start your productive group now!",
+                            style: TextStyle(
+                                fontSize: 14, color: Color(0xFF127369)),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      ],
     );
   }
 }
